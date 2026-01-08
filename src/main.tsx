1db2965c6bcaa20dde681fda3687569b04138de1
David Maman <dmaman86@gmail.com>
2026-01-08 10:37:45 +0200

    perf: remove Bootstrap dependency and replace with MUI components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import { App } from "./app";
 
 const divRoot = document.querySelector("#root");
